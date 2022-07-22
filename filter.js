57aa1ab2a2e62fbf67435c5b0664480227f1e398
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-22 11:41:11 +0600

    new

--- filter.js
+++ filter.js
@@ -1,1 +1,11 @@
-console.log("shakil")
+//this is filter function to helper for react filter 
+ const age = [32,33,16,40];
+
+ const filterager = age.filter((age)=>{
+   return age >= 18;
+ })
+
+ console.log(filterager)
+
+
+ 
