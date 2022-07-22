e852c0fbade4af0c34961fc20ee84b79d9f35790
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-22 12:09:58 +0600

    adidng

---
+++ includes.js
@@ -0,0 +1,12 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// const result = fruits.includes("Mango");
+
+// console.log(result);
+
+
+const number = [10,20,30,40,50,60]
+
+const newResult =number.includes(30);
+
+console.log(newResult);
