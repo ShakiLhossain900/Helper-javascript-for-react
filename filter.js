b4bc5c5f685616fc26ad327be9332d1401d1be3f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-22 12:00:59 +0600

    new

--- filter.js
+++ filter.js
@@ -1,11 +1,16 @@
-//this is filter function to helper for react filter 
- const age = [32,33,16,40];
+// //this is filter function to helper for react filter
+//  const age = [32,33,16,40];
 
- const filterager = age.filter((age)=>{
-   return age >= 18;
- })
+//  const filterager = age.filter((age)=>{
+//    return age >= 18;
+//  })
 
- console.log(filterager)
+//  console.log(filterager)
 
+const number = [10, 20, 30, 40];
 
- 
+const filterFunction = (num) => {
+  return num>20;
+};
+const filterNumber = number.filter(filterFunction);
+console.log(filterNumber);
