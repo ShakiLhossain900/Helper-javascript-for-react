138d00426eaafb1dcac99aee315cd28e3f213174
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-30 07:28:56 +0600

    window function also done

---
+++ eventListener.js
@@ -0,0 +1,30 @@
+
+// // in addEventListener arrow function can't work
+// document.getElementById("myBtn").addEventListener("click", myFunction);
+
+// function myFunction  (){
+//   alert("Hello world i am already ready for the work");
+// };
+
+
+
+// const shakil = document.getElementById('myBtn');
+
+// shakil.addEventListener("click",dispayDate);
+
+
+// function dispayDate() {
+//     const dateconnection = document.getElementById('demo');
+//      dateconnection.innerHTML = Date();
+// }
+
+
+
+// if i want to change the size of the window than math random will be change with every single size.
+
+// window.addEventListener('resize',mathrandom)
+
+// function mathrandom(){
+//     const pconneciton = document.getElementById('demo');
+//     pconneciton.innerHTML = Math.random();
+// }
