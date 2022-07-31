02b16f88e6f8872704b92994e89b72232626c69c
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-31 22:49:20 +0600

    adding for in and for each

---
+++ ForIn.js
@@ -0,0 +1,42 @@
+// alert("shakil you are the king !!!! please please belive on yourself");
+
+// this loop will be the for in statement loops through the properties of the object .
+
+// const person = {fname: 'John', lname: 'Doe', age:25}
+
+// let txt ="";
+// for(let x in person){
+//     //txt += person[x] + " ";
+//     txt = txt + person[x] + " ";
+// }
+// console.log(txt);
+
+// const employe = {e1:'SHAKIL', e2:"sadik", e3:"mahbub_alahi"}
+
+// let emp = "";
+// for(let x in employe){
+//   emp = emp+ employe[x]+ " " + "/";
+// }
+// console.log(emp);
+
+
+
+
+// const numbers = [45, 4, 9, 16, 25];
+
+// let txt = "";
+// for (let x in numbers) {
+//   txt += numbers[x] + "<br/>"; 
+// }
+// console.log(txt);
+
+let text = "";
+const fruits = ["apple", "orange", "cherry"];
+
+fruits.forEach(myFunction);
+
+ 
+function myFunction(item, index) {
+  text += index + ": " + item ; 
+}
+console.log(text);
